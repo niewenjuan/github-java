98479836212b5a40a817e8a205d6f26325c7e347
paas <paas@cse-codegen-backend-1254275021-33rv3>
2018-05-18 06:37:42 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/githubjava/GithubjavaApplication.java
@@ -0,0 +1,11 @@
+package com.service.githubjava;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class GithubjavaApplication {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
+        BeanUtils.init();
+    }
+}
