7b57cac8c6b85746ddd0affa62b0506b4d68e7f2
paas <paas@cse-codegen-backend-2379297868-76m8q>
2018-08-14 02:01:06 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cseuiprojectn27097/CSEUIPROJECTN27097Application.java
@@ -0,0 +1,11 @@
+package com.service.cseuiprojectn27097;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class CSEUIPROJECTN27097Application {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
+        BeanUtils.init();
+    }
+}
---
+++ src/main/java/com/service/cseuiprojectn27097/controller/Cseuiprojectn27097Delegate.java
@@ -0,0 +1,14 @@
+package com.service.cseuiprojectn27097.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Cseuiprojectn27097Delegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
